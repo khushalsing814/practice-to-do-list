2efa60cea79e615858269851ddd6cace803d2d28
Khushal Singh <98296678+khushalsing814@users.noreply.github.com>
2023-11-24 14:27:14 +0530

    make-design

--- src/components/todo_list.js
+++ src/components/todo_list.js
@@ -1,40 +1,30 @@
 import React from 'react'
 
 function Todo_list() {
-  return (
-  <>
-  <table class="table table-striped">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
-  </>
-  )
+    return (
+        <>
+            <div className='container'>
+                <div className='button_parent'>
+                    <input type='search' placeholder='full name'/>
+                    <button className='btn btn-success'>add</button>
+                </div>
+                <table className="table table-striped ">
+                    <thead>
+                        <tr>
+                            <th scope="col">S.no</th>
+                            <th scope="col">Full Name</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Manu</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </>
+    )
 }
 
 export default Todo_list
